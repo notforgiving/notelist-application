659e2b6e6af49b927b38150828c5716c2499bc6e
notforgiving <notforgiving@list.ru>
2021-01-06 15:54:34 +0300

    comb

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useSelector } from "react-redux";
-import { useEffect } from "react";
 import "./App.css";
 
 import Loginform from "./components/LoginForm";
@@ -7,7 +6,6 @@ import Notelist from "./components/Notelist";
 
 function App() {
   const { auth } = useSelector((state) => state);
-  useEffect(() => {}, [auth.login]);
   return <div className="App">{auth.login ? <Notelist /> : <Loginform />}</div>;
 }
 
